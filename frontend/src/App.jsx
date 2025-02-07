ed5402f9b5b300297b93960d1712938e235f071f
Shivanik4305 <shivanikatkar04@gmail.com>
2025-02-07 19:19:19 +0530

    Resolved merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./component/Navbar/Navbar";
 import Footer from "./component/Footer/Footer"
-import Home from "./pages/Buyer/Home"
+
 import RoutesComponent from "./routes/route";
 const App = () => {
   return (
