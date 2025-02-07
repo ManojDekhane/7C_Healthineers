7c7225b340982b1af02d2cf744471c07ec14f169
ManojDekhane <manojdekhane3@gmail.com>
2025-02-07 17:57:23 +0530

    Authentication with Google Firebase

--- frontend/src/components/auth/AdminDashboard.jsx
+++ frontend/src/components/auth/AdminDashboard.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { doSignOut } from '../../firebase/auth';
 
 const AdminDashboard = () => {
     return (
-        <>
-            <div>
-                <p className='font-bold text-center justify-center'>Dashboard</p>
-            </div>
-        </>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <p className="font-bold text-xl">Admin Dashboard</p>
+            
+            <button
+                onClick={doSignOut}
+                className="mt-4 px-4 py-2 bg-red-600 text-white font-medium rounded-lg hover:bg-red-700 transition duration-300"
+            >
+                Logout
+            </button>
+        </div>
+    );
+};
 
-    )
-}
-
-export default AdminDashboard
+export default AdminDashboard;
