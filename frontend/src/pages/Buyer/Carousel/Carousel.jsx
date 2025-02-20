c8ac6898b5470c86c4b671c0bd6e51faae8df988
Shivanik4305 <shivanikatkar04@gmail.com>
2025-02-20 08:07:40 +0530

    Corusol changes,productnavbar,pathology consumable changes

--- frontend/src/pages/Buyer/Carousel/Carousel.jsx
+++ frontend/src/pages/Buyer/Carousel/Carousel.jsx
@@ -26,7 +26,7 @@ const products = [
 
 const Carousel = () => {
   return (
-    <div className="flex justify-center items-center bg-gray-100 py-10 w-full mt-30">
+    <div className="flex justify-center items-center bg-gray-100 py-10 w-full ">
       <Swiper
         effect="coverflow"
         grabCursor={true}
